6bab23f36c7711c9c8c9c2a393ddfedf80d9fda7
Ashwin Seshadri <oklash@hotmail.com>
2018-02-24 19:23:37 -0800

    Adding the project to git.

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const indexHTML = (() => {
+  return fs.readFileSync(path.resolve(__dirname,'./index.html'), 'utf-8');
+})();
+
+app.use('/dist', express.static(path.resolve(__dirname, './dist/')));
+
+require('./build/dev-server.js')(app);
+
+app.get('*', (req, res) => {
+  res.write(indexHTML);
+  res.end();
+})
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log(`server started listening at http://localhost:${port}`);
+});
